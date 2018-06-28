bc6c6464c0c0b760658a61dac1dcac52e5d50f2d
AndreyFly <myhaandrey111@gmail.com>
2018-06-28 15:45:43 +0300

    Create 2 cats

--- Hello.java
+++ Hello.java
@@ -4,6 +4,9 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("123");
-
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+        System.out.println(cat1);
+        System.out.println(cat2);
     }
 }
