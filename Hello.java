9169d1b130a9d72af175d8457ae3de99907665f1
AndreyFly <myhaandrey111@gmail.com>
2018-07-07 12:29:08 +0300

    Load image(new File(...)) and JFrame(add Background)

--- Hello.java
+++ Hello.java
@@ -1,10 +1,34 @@
 package package1;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Hello {
     public static void main(String[] args) {
-    Cat cat1 = new Cat();
+       File file = new File("C:\\Users\\myxa\\desktop","1.bmp");
+       BufferedImage man1 = null;
+
+       try {
+           man1 = ImageIO.read(file);
+       }
+       catch (IOException e){
+           e.printStackTrace();
+       }
+        JFrame jFrame = new JFrame();
 
 
-        System.out.println(cat1);
+
+
+
+        jFrame.setContentPane(new ImageFrame(man1));
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setSize(300,300);
+        jFrame.setVisible(true);
     }
+
 }
+
